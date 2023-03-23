a2f449ce24636b2902d6489dc5bf66b5e7501523
ahmed <ahmed.lehri99@gmail.com>
2023-03-23 07:27:48 +0400

    update

---
+++ subject.js
@@ -0,0 +1,78 @@
+let  subjects= [{
+
+    lesson: "Art and Drawing",
+    location: 'UAE public school',
+    price: "99",
+    spaces: 9,
+    image: "images/art.jpg"
+}, {
+    lesson: "Science Experiments",
+    location: 'UAE public school',
+    price: "110",
+    spaces: 15,
+    image: "images/scienceExperiments.jpg"
+}, {
+    lesson: "Fitness and Sportsmanship",
+    location: 'GEMS world academy Dubai',
+    price: "345",
+    spaces: 15,
+    image: "images/sports.jpg"
+}, {
+    lesson: "Photography",
+    location: 'Repton School Dubai',
+    price: "75",
+    spaces: 8,
+    image: "images/photography.jpg"
+}, {
+    lesson: "Music theory and composition",
+    location: 'Kings School Dubai',
+    price: "120",
+    spaces: 1,
+    image: "images/music.jpg"
+}, {
+    lesson: "Mathematics Puzzles and Games",
+    location: 'Nord Anglia Intl School Dubai',
+    price: "145",
+    spaces: 7,
+    image: "images/mathPuzzle.jpg"
+}, {
+    lesson: "History of Ancient Civilization",
+    location: 'Gems Wellington International School',
+    price: "100",
+    spaces: 0,
+    image: "images/historyAncient.jpg"
+}, {
+    lesson: "Geography and World cultures",
+    location: 'British School',
+    price: "80",
+    spaces: 17,
+    image: "images/geography.png"
+}, {
+    lesson: "Learn French",
+    location: 'Jumeirah Language School',
+    price: "150",
+    spaces: 13,
+    image: "images/french.jpg"
+
+},{
+    lesson: "Public Speaking and Debate",
+    location: 'Kings School',
+    price: "45",
+    spaces: 10,
+    image: "images/debate.png"
+
+}, {
+    lesson: "Coding and Programming",
+    location: 'JESS primary School',
+    price: "250",
+    spaces: 10,
+    image: "images/coding.jpg"
+
+},{
+    lesson: "Environmental Science",
+    location: 'Repton School Dubai',
+    price: "70",
+    spaces: 20,
+    image: "images/environmentalScience.jpg"
+
+}, ]
