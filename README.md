cf74b31d7caee5850bce62072bef6f051ca17777
ahmed <ahmed.lehri99@gmail.com>
2023-03-24 16:45:04 +0400

    LU

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Assignment
 Vue.js sort
+https://github.com/Ahmedbilal1999/Assignment.git
+https://ahmedbilal1999.github.io/Assignment/
--- subject.js
+++ subject.js
@@ -3,76 +3,76 @@ let  subjects= [{
     lesson: "Art and Drawing",
     location: 'UAE public school',
     price: "99",
-    spaces: 9,
+    spaces: 5,
     image: "images/art.jpg"
 }, {
     lesson: "Science Experiments",
     location: 'UAE public school',
     price: "110",
-    spaces: 15,
+    spaces: 5,
     image: "images/scienceExperiments.jpg"
 }, {
     lesson: "Fitness and Sportsmanship",
     location: 'GEMS world academy Dubai',
     price: "345",
-    spaces: 15,
+    spaces: 5,
     image: "images/sports.jpg"
 }, {
     lesson: "Photography",
     location: 'Repton School Dubai',
     price: "75",
-    spaces: 8,
+    spaces: 5,
     image: "images/photography.jpg"
 }, {
     lesson: "Music theory and composition",
     location: 'Kings School Dubai',
     price: "120",
-    spaces: 1,
+    spaces: 5,
     image: "images/music.jpg"
 }, {
     lesson: "Mathematics Puzzles and Games",
     location: 'Nord Anglia Intl School Dubai',
     price: "145",
-    spaces: 7,
+    spaces: 5,
     image: "images/mathPuzzle.jpg"
 }, {
     lesson: "History of Ancient Civilization",
     location: 'Gems Wellington International School',
     price: "100",
-    spaces: 0,
+    spaces: 5,
     image: "images/historyAncient.jpg"
 }, {
     lesson: "Geography and World cultures",
     location: 'British School',
     price: "80",
-    spaces: 17,
+    spaces: 5,
     image: "images/geography.png"
 }, {
     lesson: "Learn French",
     location: 'Jumeirah Language School',
     price: "150",
-    spaces: 13,
+    spaces: 5,
     image: "images/french.jpg"
 
 },{
     lesson: "Public Speaking and Debate",
     location: 'Kings School',
     price: "45",
-    spaces: 10,
+    spaces: 5,
     image: "images/debate.png"
 
 }, {
     lesson: "Coding and Programming",
     location: 'JESS primary School',
     price: "250",
-    spaces: 10,
+    spaces: 5,
     image: "images/coding.jpg"
 
 },{
     lesson: "Environmental Science",
     location: 'Repton School Dubai',
     price: "70",
-    spaces: 20,
+    spaces: 5,
     image: "images/environmentalScience.jpg"
 
 }, ]
